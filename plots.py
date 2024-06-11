739fef1e5d5fee462d0465e037b55fd117ed691a
foteinipapadopoulou <fotinipapad@hotmail.com>
2024-06-11 11:43:16 +0200

    plot fitness mean values

--- plots.py
+++ plots.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-from utils import read_list
+from utils import read_a_multilist, read_list
 
 
 def load_generation_data(PATH):
@@ -45,5 +45,46 @@ def plot_boxplot_generations_per_rates():
     plot_boxplot(df)
 
 
+def calculate_average_fitness_per_generation(all_runs):
+    max_generations = max(len(run) for run in all_runs)
+    averages = []
+
+    for i in range(max_generations):
+        gen_values = [float(run[i]) for run in all_runs if i < len(run)]
+        if gen_values:
+            averages.append(sum(gen_values) / len(gen_values))
+        else:
+            averages.append(None)
+
+    return averages
+
+
+def plot_fitness(PATH, name):
+    elite = [True, False]
+    local_search = [False]
+    plt.figure(figsize=(10, 6))
+    for elite_flag in elite:
+        for local_search_flag in local_search:
+            fitnesses = read_a_multilist(f'{PATH}{name}/', name, 'fitness_mean_histories',
+                                         f'local_search_{local_search_flag}_elite_{elite_flag}')
+            # calculate the average fitnesses per generation for every run
+            average_fitnesses = calculate_average_fitness_per_generation(fitnesses)
+            label = f'Local Search: {local_search_flag}, Elite: {elite_flag}'
+            plt.plot(average_fitnesses, label=label)
+    plt.legend()
+    plt.xlabel('Generation')
+    plt.ylabel('Average Mean Fitness population')
+    plt.title(f'Average Mean Fitness population per Generation for {name} twodoku')
+    plt.show()
+
+
+def plot_fitness_lines_per_puzzle():
+    PATH = './results/Experiment with easy/'
+    easy_puzzle_names = ['easy_1', 'easy_2', 'easy_3']
+
+    for name in easy_puzzle_names:
+        plot_fitness(PATH, name)
+
 if __name__ == "__main__":
     plot_boxplot_generations_per_rates()
+    plot_fitness_lines_per_puzzle()
